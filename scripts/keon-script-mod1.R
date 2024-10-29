348134ad4fe7df446fd8cf7d5ae1ab1c25e99131
keondibley <keondibley@umail.ucsb.edu>
2024-10-29 02:46:09 -0700

    Add files via upload
    Bullet point 2 of question 3

---
+++ scripts/keon-script-mod1.R
@@ -0,0 +1,140 @@
+# Choose a larger number (more than ten) of top predictive proteins using each selection method
+# We will try choosing 25 proteins for each method!
+
+
+library(tidyverse)
+library(infer)
+library(randomForest)
+library(tidymodels)
+library(modelr)
+library(yardstick)
+load('data/biomarker-clean.RData')
+
+## MULTIPLE TESTING
+####################
+
+# function to compute tests
+test_fn <- function(.df){
+  t_test(.df, 
+         formula = level ~ group,
+         order = c('ASD', 'TD'),
+         alternative = 'two-sided',
+         var.equal = F)
+}
+
+ttests_out <- biomarker_clean %>%
+  # drop ADOS score
+  select(-ados) %>%
+  # arrange in long format
+  pivot_longer(-group, 
+               names_to = 'protein', 
+               values_to = 'level') %>%
+  # nest by protein
+  nest(data = c(level, group)) %>% 
+  # compute t tests
+  mutate(ttest = map(data, test_fn)) %>%
+  unnest(ttest) %>%
+  # sort by p-value
+  arrange(p_value) %>%
+  # multiple testing correction
+  mutate(m = n(),
+         hm = log(m) + 1/(2*m) - digamma(1),
+         rank = row_number(),
+         p.adj = m*hm*p_value/rank)
+
+#ttests_out
+
+# select significant proteins
+proteins_s1 <- ttests_out %>%
+  slice_min(p.adj, n = 25) %>%
+  pull(protein)
+
+#proteins_s1
+
+## RANDOM FOREST
+##################
+
+# store predictors and response separately
+predictors <- biomarker_clean %>%
+  select(-c(group, ados))
+
+response <- biomarker_clean %>% pull(group) %>% factor()
+
+# fit RF
+set.seed(101422)
+rf_out <- randomForest(x = predictors, 
+                       y = response, 
+                       ntree = 1000, 
+                       importance = T)
+
+# check errors
+rf_out$confusion
+
+# compute importance scores
+proteins_s2 <- rf_out$importance %>% 
+  as_tibble() %>%
+  mutate(protein = rownames(rf_out$importance)) %>%
+  slice_max(MeanDecreaseGini, n = 25) %>%
+  pull(protein)
+
+proteins_s2
+
+## LOGISTIC REGRESSION
+#######################
+
+# select subset of interest
+proteins_sstar <- intersect(proteins_s1, proteins_s2)
+
+proteins_sstar
+
+biomarker_sstar <- biomarker_clean %>%
+  select(group, any_of(proteins_sstar)) %>%
+  mutate(class = (group == 'ASD')) %>%
+  select(-group)
+
+# partition into training and test set
+set.seed(101422)
+biomarker_split <- biomarker_sstar %>%
+  initial_split(prop = 0.8)
+
+# fit logistic regression model to training set
+fit <- glm(class ~ ., 
+           data = training(biomarker_split), 
+           family = 'binomial')
+
+# evaluate errors on test set
+class_metrics <- metric_set(sensitivity, 
+                            specificity, 
+                            accuracy,
+                            roc_auc)
+
+testing(biomarker_split) %>%
+  add_predictions(fit, type = 'response') %>%
+  mutate(est = as.factor(pred > 0.5), tr_c = as.factor(class)) %>%
+  class_metrics(estimate = est,
+                truth = tr_c, pred,
+                event_level = 'second')
+
+
+
+# Original intersected proteins: "DERM"  "RELT"  "IgD"   "FSTL1"
+
+# Model metrics on original analysis (on testing set): 
+# Sensitivity = 0.875
+# Specificity = 0.8
+# Accuracy = 0.839
+# Roc_auc = 0.908
+
+
+# New intersected proteins: "DERM", "RELT", "Calcineurin", "IgD", "PTN", 
+# "FSTL1", "MAPK2", "TGF-b R III", "MMP-2", "gp130, soluble", "Notch 1", "ALCAM", "MATN2"
+
+# Model metrics on larger model: 
+# Sensitivity = 0.812
+# Specificity = 0.867
+# Accuracy = 0.839
+# Roc_auc = 0.946
+
+# Accuracy is the same, and ROC_AUC is higher, which suggest that the large model performed better. 
+# Sensitivity decreased while specificity increased, suggesting more false positives and fewer false negatives. 
+
